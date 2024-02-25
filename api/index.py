c6388143dcac611abe804ca69b7dc80fdb9e50d1
MacBook Pro <misterfighter1990@gmail.com>
2024-02-25 03:07:19 -0800

    adde

--- api/index.py
+++ api/index.py
@@ -2,8 +2,27 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import render_template
+from psycopg2 import connect
 
 app = Flask(__name__)
+
+db = connect(
+    dbname="rqskigmw",
+    user="rqskigmw",
+    password="Ph3vQzCXoigc-g-dYHrVwYTD7lMPkeJn",
+    host="surus.db.elephantsql.com",
+    port="5432"
+)
+
+cursor = db.cursor()
+
+cursor.execute("SELECT version();")
+
+record = cursor.fetchone()
+print("You are connected to - ", record)
+
+cursor.close()
+db.close()
 
 @app.route('/')
 def main():
@@ -31,7 +50,6 @@ def forgotPassword():
         "message": "forgot password",
         "status": True
     })
-
 
 if __name__ == "__main__":
     app.run()
