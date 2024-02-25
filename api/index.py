7c61a7dbb67bb441567af03701dcaac3295b82e8
MacBook Pro <misterfighter1990@gmail.com>
2024-02-24 23:53:08 -0800

    post req

--- api/index.py
+++ api/index.py
@@ -3,21 +3,21 @@ from flask import jsonify
 
 app = Flask(__name__)
 
-@app.route('/sign-in')
+@app.route('/sign-in', methods=['POST'])
 def signIn():
     return jsonify({
         "message": "sign in",
         "status": True
     })
 
-@app.route('/sign-up')
+@app.route('/sign-up', methods=['POST'])
 def signUp():
     return jsonify({
         "message": "sign up",
         "status": True
     })
 
-@app.route('/forgot-password')
+@app.route('/forgot-password', methods=['POST'])
 def forgotPassword():
     return jsonify({
         "message": "forgot password",
