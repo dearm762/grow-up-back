e4d307fef1b4ac88fbf40562e78d58194a17a4f5
MacBook Pro <misterfighter1990@gmail.com>
2024-02-25 00:06:53 -0800

    test res body

--- api/index.py
+++ api/index.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/sign-in', methods=['POST'])
 def signIn():
+    request_data = request.get_json()
     return jsonify({
         "message": "sign in",
-        "status": True
+        "status": True,
+        "data": request_data
     })
 
 @app.route('/sign-up', methods=['POST'])
